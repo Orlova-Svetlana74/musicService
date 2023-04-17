8af5693c4d0f2f0b1a2893ce00af0336c8fb5ac5
Orlova-Svetlana74 <sorlova#mail.ru>
2023-04-18 00:32:16 +0300

    init1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg'
+import './App.css'
+import Menu from './components/menu'
+import Player from './components/player'
+import Bar from './components/bar'
+
+function App() {
+  return (
+    <div className="App">
+
+        <Menu />
+        <Player />
+        <Bar />
+      
+    </div>
+  )
+}
+export default App
